55f574366014bff3a2cfe9a0e33d75f75779a51c
JoralmoPro <JoralmoPro@gmail.com>
2020-08-30 21:33:03 -0500

    Hola mundo 😎

--- public/js/app.js
+++ public/js/app.js
@@ -37526,7 +37526,7 @@ var staticRenderFns = [
         _c("div", { staticClass: "col-md-8" }, [
           _c("div", { staticClass: "card" }, [
             _c("div", { staticClass: "card-header" }, [
-              _vm._v("Example Component")
+              _vm._v("Componente de ejemplo")
             ]),
             _vm._v(" "),
             _c("div", { staticClass: "card-body" }, [
